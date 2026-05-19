36ad270c9ec242da70afb266dca4db91c2e936a6
Ryan Hoshor <ryan@hoshor.me>
2026-05-18 19:16:36 -0600

    less padding

--- components/StickyToolbar.vue
+++ components/StickyToolbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sticky top-2 mb-4" :class="[isScrolled ? 'z-50' : '']">
+  <div class="sticky top-2 mb-2" :class="[isScrolled ? 'z-50' : '']">
     <!-- Glass effect toolbar -->
     <div
       class="flex items-center justify-between gap-2 py-2 mx-auto transition-all duration-200"
