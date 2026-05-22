dec93b5de338433b0fe3e4b90a058d400f22af1e
Ryan Hoshor <ryan@hoshor.me>
2026-05-22 10:28:19 -0600

    padding + border

--- pages/notes/[id].vue
+++ pages/notes/[id].vue
@@ -80,8 +80,7 @@
       </template>
 
       <!-- Editor mode (new or editing) -->
-      <div v-else class="md:card flex-1 flex flex-col min-h-0 -mt-6">
-        <hr class="mb-4 border-gray-200 dark:border-gray-700" />
+      <div v-else class="md:card flex-1 flex flex-col min-h-0">
         <!-- Content Area -->
         <div class="flex-1 min-h-0 h-full">
           <textarea v-model="editorContent" placeholder="Write your markdown here..."
